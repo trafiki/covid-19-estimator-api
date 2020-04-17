4cda588b13e11a983337b00db74e8c44da7d1354
Machine <babsake@gmail.com>
2020-04-17 15:12:27 +0100

    Add log route

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const compression = require('compression');
 const cors = require('cors');
 const helmet = require('helmet');
 const jsonToXml = require('jsontoxml');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 const covid19ImpactEstimator = require('./estimator');
 
 const app = express();
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs.log'), { flags: 'a' })
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(compression());
+app.use(morgan(':method :url :status :res[-] :response-time ms', { stream: accessLogStream }));
 
 // HOME
 app.get('/', (req, res) => res.send('Welcome to the Covid 19 estimator API'));
@@ -40,6 +47,14 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
   const stats = covid19ImpactEstimator(req.body);
   res.header('content-Type', 'application/xml; charset=UTF-8');
   res.send(jsonToXml(stats));
+});
+
+app.get('/api/v1/on-covid-19/logs', (req, res) => {
+  fs.readFile('./logs.log', 'utf8', (err, data) => {
+    if(err) throw err;
+    res.set('Content-Type', 'text/plain')
+    res.send(data);
+  });
 });
 
 const port = process.env.PORT || 3000;
